6d2b3f723d6e5a1284fef16b0faf1b9b1790f6f5
tejeshreddy <tejeshreddy111@gmail.com>
2020-12-06 22:30:55 +0530

    feat: container_with_most_water.py

--- LeetCode/Python/container_with_most_water.py
+++ LeetCode/Python/container_with_most_water.py
@@ -6,6 +6,7 @@ Space: O(1)
 Source: https://leetcode.com/problems/container-with-most-water/
 Difficulty: Medium
 """
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         i, j, max_area = 0, len(height)-1, 0
