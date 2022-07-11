2c21805dea5e451ad090d3cd745188500009bfcb
Tejesh Reddy <tejeshreddy111@gmail.com>
2022-07-11 16:59:09 -0500

    Time: 80 ms (5.16%), Space: 14 MB (45.44%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,21 +1,22 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if s[0] == "0" or s is None:
+        if len(s) == 0 or s is None:
             return 0
-        
-        @lru_cache()
+
+        @lru_cache(maxsize=None)
         def dfs(string):
             if len(string) > 0:
                 if string[0] == "0":
                     return 0
             if len(string) == 1 or string == "":
                 return 1
-                
-            if int(string[0:2]) <= 26:
-                res1 = dfs(string[1:])
-                res2 = dfs(string[2:])
-                return res1 + res2
+            
+            if int(string[:2]) <= 26:
+                first = dfs(string[1:])
+                second = dfs(string[2:]) 
+                return first + second
             else:
                 return dfs(string[1:])
+
         return dfs(s)
-        
+            
