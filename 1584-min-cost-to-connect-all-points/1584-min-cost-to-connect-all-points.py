e1aadb5cb65d4c68e4fe1ebe3bef06362c45e5bc
Tejesh Reddy <tejeshreddy111@gmail.com>
2022-07-14 00:31:31 -0500

    Time: 5861 ms (16.33%), Space: 175 MB (10.08%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,37 +1,39 @@
+from heapq import heappush, heappop
+
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         N = len(points)
-        hmap = collections.defaultdict(list)
         
+        # Building the adjacency list
+        adjlist = {i: [] for i in range(N)}
         for i in range(N):
             x1, y1 = points[i]
             for j in range(i + 1, N):
                 x2, y2 = points[j]
-                dist = abs(x2 - x1) + abs(y2 - y1)
-                hmap[i].append([dist, j])
-                hmap[j].append([dist, i])
-        visited = set()
-        cost = 0
-        
-        q = [[0, 0]]
-        while len(visited) != N:
-            dist, node = heapq.heappop(q)
-            if node not in visited:
-                cost += dist
-                visited.add(node)
-                for neigh in hmap[node]:
-                    if neigh[1] not in visited:
-                        heapq.heappush(q, neigh)
-        return cost
-                
-                
-        
-        
-        
+                dist = abs(x2 - x1) + abs (y2 - y1)
+                adjlist[i].append([dist, j])
+                adjlist[j].append([dist, i])
         
                 
-                
         
+        # Prim's Implementation
+        minHeap = [[0, 0]] # [Weight, Node]
+        visit = set()
+        res = 0
         
+        while len(visit) < N:
+            w1, n1 = heappop(minHeap)
+            if n1 in visit:
+                continue
+            res += w1
+            visit.add(n1)
+            
+            for w2, n2 in adjlist[n1]:
+                if n2 in visit:
+                    continue
+                heappush(minHeap, [w2, n2])
+        
+        return res
+            
         
         
