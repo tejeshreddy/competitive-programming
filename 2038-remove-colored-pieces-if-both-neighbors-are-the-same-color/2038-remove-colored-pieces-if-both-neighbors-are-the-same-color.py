428859e1f17920fbc82fbb55d1bd42a0cc657276
Tejesh Reddy <tejeshreddy111@gmail.com>
2022-07-21 15:03:15 -0500

    Time: 392 ms (46.98%), Space: 16.1 MB (6.79%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -7,22 +7,18 @@ class Solution:
         
         def check_stack():
             nonlocal a_count, b_count, stack
-            if stack[-3] == stack[-2] == stack[-1]:
-                turn_color = stack.pop()
-                if turn_color == "A":
-                    a_count += 1
-                else:
-                    b_count += 1
+            if len(stack) >= 3:
+                if stack[-3] == stack[-2] == stack[-1]:
+                    turn_color = stack.pop()
+                    if turn_color == "A":
+                        a_count += 1
+                    else:
+                        b_count += 1
         
         for c in colors:
-            if len(stack) < 3:
-                stack.append(c)
-            else:
-                check_stack()
-                stack.append(c)
-        if len(stack) >= 3:
             check_stack()
-        print(a_count, b_count)
+            stack.append(c)
+        check_stack()
         if a_count > b_count:
             return True
         return False
