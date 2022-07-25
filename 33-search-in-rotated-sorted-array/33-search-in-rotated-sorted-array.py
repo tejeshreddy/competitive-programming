199e07c72cbcfd7335f73ef8ff18c657ea7afc13
Tejesh Reddy <tejeshreddy111@gmail.com>
2022-07-24 22:28:41 -0500

    Time: 59 ms (60.93%), Space: 14.3 MB (56.22%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -8,37 +8,20 @@ class Solution:
                 l = mid + 1
             else:
                 r = mid
-        
         rotation = l
+        
         l, r = 0, len(nums) - 1
         
         while l <= r:
             mid = (l + r) // 2
             rmid = (mid + rotation) % len(nums)
+            
             if nums[rmid] == target:
                 return rmid
-            if target > nums[rmid]:
-                l = mid + 1
-            else:
+            elif nums[rmid] > target:
                 r = mid - 1
+            else:
+                l = mid + 1
+        
         return -1
-                
         
-        
-        
-#         zi = nums.index(0)
-#         nums = nums[zi:] + nums[:zi]
-        
-#         l, r = 0, len(nums)
-        
-#         while l <= r:
-#             mid = (l + r) // 2
-#             if nums[mid] == target:
-#                 return (mid + zi) % len(nums)
-            
-#             if target < nums[mid]:
-#                 r = mid - 1
-#             else:
-#                 l = mid + 1
-#         return -1
-
