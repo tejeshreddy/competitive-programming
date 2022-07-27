98645f8af20d19a7a339c71572c611ee1c0a5efc
Tejesh Reddy <tejeshreddy111@gmail.com>
2022-07-27 17:58:12 -0500

    Time: 945 ms (69.14%), Space: 19.1 MB (27.98%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.py
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -1,16 +1,12 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         result = [0] * (n + 1)
-        hmap = collections.defaultdict(list)
         
-        for t in trust:
-            hmap[t[1]].append(t[0])
-            result[t[0]] += 1
+        for i, j in trust:
+            result[i] -= 1
+            result[j] += 1
         
-        
-        for k, v in enumerate(result):
-            if v == 0 and k != 0 and len(hmap[k]) == n - 1:
-                return k
-
+        for i in range(1, n + 1):
+            if result[i] == n - 1:
+                return i
         return -1
-        
