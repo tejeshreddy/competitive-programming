b2fd7ad93971535e8eba826f649dcf9dfcc27868
Tejesh Reddy <tejeshreddy111@gmail.com>
2022-08-26 18:04:22 -0700

    Time: 171 ms (44.39%), Space: 17.2 MB (88.84%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,16 +1,24 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        
         hmap = collections.defaultdict(list)
+        for s in strs:
+            ss = "".join(sorted(s))
+            hmap[ss] = hmap[ss] + [s]
+        return list(hmap.values())  
         
-        for word in strs:
-            mapping = [0] * 26
-            for c in word:
-                mapping[ord(c) - ord("a")] += 1
-            mapping = ["0" * (2 - len(str(i))) + str(i) for i in mapping]
-            hmap["".join(mapping)].append(word)
-        return list(hmap.values())
+        
+#         hmap = collections.defaultdict(list)
+        
+#         for word in strs:
+#             mapping = [0] * 26
+#             for c in word:
+#                 mapping[ord(c) - ord("a")] += 1
+#             mapping = ["0" * (2 - len(str(i))) + str(i) for i in mapping]
+#             hmap["".join(mapping)].append(word)
+#         return list(hmap.values())
+    
+    
         
         
         
