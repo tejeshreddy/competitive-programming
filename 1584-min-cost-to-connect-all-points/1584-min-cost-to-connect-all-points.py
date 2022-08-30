8a7d6778b982caf8d1dc4bb55fb58f570b1a9e62
Tejesh Reddy <tejeshreddy111@gmail.com>
2022-08-29 17:42:09 -0700

    Time: 6371 ms (13.83%), Space: 173.6 MB (21.18%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -2,33 +2,35 @@ class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         
         graph = collections.defaultdict(list)
-        N = len(points)
         
-        for i in range(N):
-            x1, y1 = points[i]
-            for j in range(i + 1, N):
-                x2, y2 = points[j]
-                dist = abs(x2 - x1) + abs(y2 - y1)
-                graph[i].append((dist, j))
-                graph[j].append((dist, i))
-        
-        queue = [(0, 0)]
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                graph[i].append([dist, j])
+                graph[j].append([dist, i])
+
+        q = [[0, 0]] # [Weight, Node]
         visited = set()
-        result = 0
+        total = 0
         
-        while len(visited) != N:
-            w1, p1 = heapq.heappop(queue)
-            if p1 not in visited:
-                result += w1
-                visited.add(p1)
+        while len(visited) != len(points):
+            w, n = heapq.heappop(q)
+            if n in visited:
+                continue
+            visited.add(n)
+            total += w
+            for w2, n2 in graph[n]:
+                if n2 not in visited:
+                    heapq.heappush(q, [w2, n2])
+        return total
             
-            for points in graph[p1]:
-                w2, p2 = points
-                if p2 not in visited:
-                    heapq.heappush(queue, (w2, p2))
-                
-        return result
             
-                
-                
             
+        
+        
+        
+        
+        
+                
+                
+                
