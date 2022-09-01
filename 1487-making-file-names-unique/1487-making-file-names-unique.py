ab20f3853c7148e22b87c4cd182407b1688462e1
Tejesh Reddy <tejeshreddy111@gmail.com>
2022-09-01 11:24:38 -0700

    Time: 784 ms (9.60%), Space: 26.9 MB (82.49%) - LeetHub

--- 1487-making-file-names-unique/1487-making-file-names-unique.py
+++ 1487-making-file-names-unique/1487-making-file-names-unique.py
@@ -2,15 +2,12 @@ class Solution:
     def getFolderNames(self, names: List[str]) -> List[str]:
         used, hmap = set(), collections.defaultdict(int)
         result = []
-
         for name in names:
             k = hmap[name]
             current = name
-            print(current)
             while current in used:
                 k += 1
                 current = "%s(%d)" % (name, k)
-
             hmap[name] = k
             result.append(current)
             used.add(current)
