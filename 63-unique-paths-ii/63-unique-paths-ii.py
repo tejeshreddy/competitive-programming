30455d341e86cba01d331d6b27a49497f9b65fbb
Tejesh Reddy <tejeshreddy111@gmail.com>
2022-09-05 18:23:21 -0700

    Time: 51 ms (81.07%), Space: 14.1 MB (43.49%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        rows, cols = len(obstacleGrid), len(obstacleGrid[0])
+        
+        def dfs(r, c, hmap):
+            if r >= rows or c >= cols:
+                return 0
+            if obstacleGrid[r][c] == 1:
+                return 0
+            if r == rows - 1 and c == cols - 1:
+                return 1
+            if (r, c) in hmap:
+                return hmap[(r, c)]
+            
+            down = dfs(r + 1, c, hmap)
+            right = dfs(r, c + 1, hmap)
+            
+            hmap[(r, c)] = down + right
+            return hmap[(r, c)]
+        
+        return dfs(0, 0, {})
+        
+        
+        
