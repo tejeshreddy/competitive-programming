ecc4ee47f60cbf6ba99d70f6f3df69c6483e8e3e
Tejesh Reddy <tejeshreddy111@gmail.com>
2022-09-20 12:13:02 -0700

    Time: 9596 ms (5.00%), Space: 39.9 MB (33.59%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,17 +1,20 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        len_n1 = len(nums1)
-        len_n2 = len(nums2)
-        max_len = 0
+        len1, len2 = len(nums1), len(nums2)
+        grid = [[0 for _ in range(len2 + 1)] for _ in range(len1 + 1)]
+        result = 0
         
-        grid = [[0 for _ in range(len_n2 + 1)] for _ in range(len_n1 + 1)]
-        
-        for r in range(len_n1 - 1, -1, -1):
-            for c in range(len_n2 - 1, -1, -1):
+        for r in range(len1 - 1, -1, -1):
+            for c in range(len2 - 1, -1, -1):
                 if nums1[r] == nums2[c]:
                     grid[r][c] = 1 + grid[r + 1][c + 1]
-                    max_len = max(max_len, grid[r][c])
-        return max_len
+                # else:
+                #     grid[r][c] = max(grid[r + 1][c], grid[r][c + 1])
+                result = max(grid[r][c], result)
+        return result
+                
                     
+                    
+        
         
         
