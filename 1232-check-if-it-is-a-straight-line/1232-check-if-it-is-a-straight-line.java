939a90e4987d2c24a911356db08f68e10f0ffdc7
Tejesh Reddy <tejeshreddy111@gmail.com>
2023-06-05 07:42:11 -0700

    Time: 0 ms (100.00%), Space: 42.8 MB (20.98%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Returns the delta Y.
+    int getYDiff(int[] a, int[] b) {
+        return a[1] - b[1];
+    }
+    
+    // Returns the delta X.
+    int getXDiff(int[] a, int[] b) {
+        return a[0] - b[0];
+    }
+    
+    public boolean checkStraightLine(int[][] coordinates) {
+        int deltaY = getYDiff(coordinates[1], coordinates[0]);
+        int deltaX = getXDiff(coordinates[1], coordinates[0]);
+        
+        for (int i = 2; i < coordinates.length; i++) {
+            if (deltaY * getXDiff(coordinates[i], coordinates[0])
+                != deltaX * getYDiff(coordinates[i], coordinates[0])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
