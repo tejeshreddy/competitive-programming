c181d1b63b57da61e0a52fbbdbb4e10b1176565c
Tejesh Reddy <tejeshreddy111@gmail.com>
2023-07-26 15:24:04 -0500

    Time: 2421 ms (93.56%), Space: 30.4 MB (92.72%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -1,21 +1,11 @@
 class Solution:
     def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
-
-        # check if it's possible to reach the office on time with a given speed
-        def can_reach_on_time(speed):
-            total_time = sum( ((d + speed - 1) // speed) for d in dist[:-1]) + dist[-1] / speed
-            return total_time <= hour
-
-        # binary search to find the minimum speed
-        left, right = 1, 10**7
-        while left < right:
-            mid = (left + right) // 2
-            if can_reach_on_time(mid):
-                right = mid
+        if len(dist) >= hour + 1  : return -1
+        left,right = 1, 10 ** 7
+        while left<right:
+            mid=(left+right)//2
+            if sum([ceil(i/mid) for i in dist[:-1]])+(dist[-1]/mid) <= hour :
+                right=mid
             else:
-                left = mid + 1
-        
-        if can_reach_on_time(left):
-            return left
-        else:
-            return -1
+                left=mid+1
+        return left
