9f28e98a7706316fbdbcf0506b181817ddc1da9f
Tejesh Reddy <tejeshreddy111@gmail.com>
2023-09-08 13:50:44 -0600

    Time: 190 ms (81.25%), Space: 23.7 MB (84.30%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,23 +1,45 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        zc = 0
-        pc = 1
+        """
+        all elements are positivie -> [1, 2, 3, 4] => 24 => [24, 12, 8, 6]
+        negatives -> [-2, -3, 5, 6] -> 180 -> [-90, -60, 36, 30]
+        zeros -> [0, 2, 3, 4] -> [24, 0, 0, 0]
+        multiple zeros => [0, 2, 0, 3] -> [0, 0, 0, 0]
         
-        for n in nums:
-            if n == 0:
-                zc += 1
+        1. count the number of 0's
+        2. if zero count gt 2
+            steps
+        3 if zero count lt 2 gt 0
+            steps
+        4. normal steps
+        """
+        zero_count = 0
+        product = 1
+        
+        for number in nums:
+            if number == 0:
+                zero_count += 1
             else:
-                pc *= n
+                product *= number
+        
         result = []
-        if zc == 0:
-            for n in nums:
-                result.append(pc // n)
-        elif zc == 1:
-            for n in nums:
-                if n != 0:
-                    result.append(0)
-                else:
-                    result.append(pc)
+        
+        if zero_count > 0:
+            if zero_count >= 2:
+                result = [0] * len(nums)
+            else:
+                for number in nums:
+                    if number == 0:
+                        result.append(product)
+                    else:
+                        result.append(0)
         else:
-            result = [0] * len(nums)
+            for number in nums:
+                result.append(int(product/number))
+        
         return result
+            
+        
+                
+            
+        
