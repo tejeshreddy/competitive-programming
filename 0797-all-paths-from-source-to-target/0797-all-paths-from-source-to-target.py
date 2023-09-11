70d42c9bbd7c08c8ee68fe8d198ba080d9391c59
Tejesh Reddy <tejeshreddy111@gmail.com>
2023-09-11 11:17:08 -0600

    Time: 78 ms (94.07%), Space: 18.1 MB (84.60%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -6,8 +6,6 @@ class Solution:
         while queue:
             path = queue.pop(0)
             for neigh in graph[path[-1]]:
-                # temp = path.copy()
-                # temp.append(neigh)
                 if neigh == len(graph) - 1:
                     result.append(path + [neigh])
                 else:
