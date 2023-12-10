0b101f3b91fefec0aecd2344f66a09d9286cbb78
Tejesh Reddy <tejeshreddy111@gmail.com>
2023-12-10 16:17:15 -0600

    Time: 6783 ms (25.72%), Space: 16.5 MB (20.41%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,22 +1,36 @@
+from typing import List
+
 class Solution:
+    def __init__(self):
+        self.rows = 0
+        self.cols = 0
+        self.board = [[]]
+
+    def dfs(cls, r, c, w, visited, rows, cols, board):
+        if w == "":
+            return True
+        if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != w[0] or (r, c) in visited:
+            return False
+        else:
+            visited.add((r, c))
+            result = (
+                cls.dfs(r + 1, c, w[1:], visited, rows, cols, board) or
+                cls.dfs(r - 1, c, w[1:], visited, rows, cols, board) or
+                cls.dfs(r, c + 1, w[1:], visited, rows, cols, board) or
+                cls.dfs(r, c - 1, w[1:], visited, rows, cols, board)
+            )
+            visited.remove((r, c))
+            return result
+
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
-        
-        def dfs(r, c, w):
-            if w == "":
-                return True
-            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != w[0] or (r, c) in visited:
-                return False
-            else:
-                visited.add((r, c))
-                result = dfs(r + 1, c, w[1:]) or dfs(r - 1, c, w[1:]) or dfs(r, c + 1, w[1:]) or dfs(r, c - 1, w[1:])
-                visited.remove((r, c))
-                return result
-        
-        for r in range(0, rows):
-            
-            for c in range(0, cols):
+        self.rows = len(board)
+        self.cols = len(board[0])
+        self.board = board
+
+        for r in range(self.rows):
+            for c in range(self.cols):
                 visited = set()
-                if board[r][c] == word[0] and dfs(r, c, word):
+                if self.board[r][c] == word[0] and self.dfs(r, c, word, visited, self.rows, self.cols, self.board):
                     return True
         return False
+
