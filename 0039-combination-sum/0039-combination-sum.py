eef4d57fc51886728643ca9b6b11e8c6ca679f7d
Tejesh Reddy <tejeshreddy111@gmail.com>
2023-12-10 15:42:21 -0600

    Time: 47 ms (95.87%), Space: 16.5 MB (14.18%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,21 @@
 class Solution:
+    def __init__(self):
+        self.result = []
+    
+    def dfs(self, candidates, start, target, current_solution):
+        if target == 0:
+            self.result.append(current_solution)
+            return
+        if target < 0:
+            return
+        for current in range(start, len(candidates)):
+            self.dfs(candidates, current, target - candidates[current], current_solution + [candidates[current]])
+    
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        result = []
+        for start in range(len(candidates)):
+            self.dfs(candidates, start, target - candidates[start], [candidates[start]])
+        return self.result
         
-        def dfs(path, candidates, start, target):
-            if target == 0:
-                result.append(path)
-                return
-            if target < 0 or start >= len(candidates):
-                return
-            
-            for i in range(start, len(candidates)):
-                dfs(path + [candidates[i]], candidates, i, target - candidates[i])
         
-        dfs([], candidates, 0, target)
-        return result
-                
         
         
